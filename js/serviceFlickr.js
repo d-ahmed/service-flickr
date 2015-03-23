8f964b6123358141486c9e70f63901ecf35c13e1
danieldaniel06 <daniel.ahmedinfo@gmail.com>
2015-03-23 10:55:56 +0100

    update

---
+++ js/serviceFlickr.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+	var commune = [];
+	$("#commune").autocomplete({
+		minLength:3,
+		source: function(request, response){
+			$.ajax({
+				url:"http://infoweb-ens/~jacquin-c/codePostal/commune.php",
+				type:'POST',
+				dataType:'json',
+				data:{"commune":$("#commune").val()},
+				success:function(data){
+					commune=[];
+					if(data){
+						$.each(data,function(i){
+							commune[i]=data[i].Ville;
+						});
+						response(commune);
+					}					
+				},
+	
+				error:function(data,status,err){
+					alert("error"+err);
+				}
+			})
+		},
+	});
+	
+
+	$('.recherche').submit(function(e){
+		e.preventDefault();
+	})
+})
